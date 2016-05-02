f20639a03372529b328deea5a362bbe5d1343b91
codingWang <379906411@qq.com>
2016-05-02 10:39:18 +0800

    增加排序算法

--- src/com/duweri/interview/Sort.java
+++ src/com/duweri/interview/Sort.java
@@ -5,11 +5,15 @@ import javax.management.Descriptor;
 public class Sort {
 
 	public static void main(String[] args) {
-		int[] testArr = {1,7,3,0,4,8,3,6,2,5,9};
-		Sort.insertSort(testArr);
+		int[] testArr = {12,27,19,39,16,49,61,40,20,94,63,13,27,31,38,43,11,44,81,29,33,88,60,80,51,4,0,1,22,9,8};
+	//	Sort.insertSort(testArr);
+//		Sort.bubbleSort(testArr);
+//		Sort.quickSort(testArr, 0, testArr.length-1);
+		Sort.selectSort(testArr);
 		Sort.display(testArr);
 	}
-	public static void display(int[] arr){
+	
+ public static void display(int[] arr){
 		for (int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i]+",");
 		}
@@ -17,7 +21,8 @@ public class Sort {
 	
 	/**
 	 * 插入排序
-	 * {@link Descriptor}先用i指向待插入数，j往前查找同时后移
+	 * 关键：1、临时变量保存待排序的数字
+	 * 		2、边比较边后移
 	 * 适用情况：序列基本有序的时候
 	 */
 	public static void insertSort(int[] arr){
@@ -42,10 +47,100 @@ public class Sort {
 	}
 	/**
 	 * 冒泡排序
+	 * 关键：1、外循环标识无序区和有序区
+	 * 		2、内循环每次将一个无序区的数字排到最前面
 	 */
 	public static void bubbleSort(int[] arr){
-		
+		for (int i = 0; i <arr.length-1; i++) {//从后往前推小的
+			for (int j = arr.length-1; j>i; j--) {
+				if (arr[j]>arr[j-1]) {
+					int temp = arr[j];//swap
+					arr[j] = arr[j-1];
+					arr[j-1] = temp;
+				}
+			}
+		}
 	}
 	
-
+	/**
+	 * 快速排序
+	 * 关键:1、选轴值
+	 * 	2、一次划分
+	 * 3、递归快排
+	 */
+	public static void quickSort(int[] arr,int start,int end){
+		if(start<end){
+			int middle = partition(arr, start, end);
+			quickSort(arr, start, middle-1);
+			quickSort(arr, middle+1, end);
+		}
+	}
+	/**
+	 * 以arr[0]为基准进行一次划分算法，左边都小于arr[0]，右边都大于arr[0]
+	 */
+	private static int partition(int[] arr,int first,int end){
+		
+		while(first < end){
+			//先扫描右端
+			while(first < end && arr[first]<arr[end]){
+				end--;
+			}
+			if(first < end){
+				int temp = arr[end];
+				arr[end] = arr[first];
+				arr[first] = temp;
+				first++;
+			}
+			//扫描左边
+			while(first < end && arr[first]<arr[end]){
+				first++;
+			}
+			if(first<end){
+				int temp = arr[end];
+				arr[end] = arr[first];
+				arr[first] = temp;
+				end--; 
+			}
+		}
+		return first;
+	}
+	
+	/**
+	 * 选择排序：
+	 * 每次遍历无序区选出最小的
+	 * @param arr
+	 */
+	public static void selectSort(int[] arr){
+		int minIndex = 0;
+		for (int i = 0; i < arr.length-1; i++) {
+			minIndex = i;//指向最小的值的角标
+			for (int j = i+1; j < arr.length; j++) {//遍历无序区
+				if(arr[j] < arr[minIndex]){
+					minIndex = j;
+				}
+			}
+			if (minIndex != i) {		//无序区遍历完了，找到最小的角标了，交换
+				int temp = arr[minIndex];
+				arr[minIndex] = arr[i];
+				arr[i] = temp;//把temp放到有序区
+			}
+		}
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
