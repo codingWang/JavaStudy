0ea9993bc98495622005c3e469cabfae1dbb6364
codingWang <379906411@qq.com>
2016-07-21 23:16:16 +0800

    date api

---
+++ src/com/duweri/interview/DateTest.java
@@ -0,0 +1,25 @@
+package com.duweri.interview;
+
+import java.util.Calendar;
+
+public class DateTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int y,m,d,h,mi,s,nowsj;
+		Calendar calendar = Calendar.getInstance();
+		y=calendar.get(Calendar.YEAR);
+		m=calendar.get(Calendar.MONTH);
+		d=calendar.get(Calendar.DATE);
+		h=calendar.get(Calendar.HOUR_OF_DAY);
+		mi=calendar.get(Calendar.MINUTE);
+		s=calendar.get(Calendar.MINUTE);
+		nowsj=y*100+m;
+		System.out.println(y+"Äê"+m+"-"+d+"-"+h+"-"+nowsj);
+		//2016Äê6-21-10-201606
+	}
+
+}
