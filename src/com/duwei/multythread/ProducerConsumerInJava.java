3d6fbae6d6308605986738d6ec28ca8e7bd2308e
codingWang <379906411@qq.com>
2016-09-18 08:58:49 +0800

    多线程之生产者消费者问题

---
+++ src/com/duwei/multythread/ProducerConsumerInJava.java
@@ -0,0 +1,90 @@
+package com.duwei.multythread;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+/**
+ * 生产者消费者问题
+ * http://developer.51cto.com/art/201508/487488.htm
+ * @author 杜伟
+ */
+public class ProducerConsumerInJava { 
+	
+	public static void main(String args[]) { 
+		System.out.println("如何在java中使用 wait and notify"); 
+		System.out.println("解决生产者消费者问题"); 
+		Queue<Integer> buffer = new LinkedList<>(); 
+		int maxSize = 10; 
+		Thread producer = new Producer(buffer, maxSize, "PRODUCER"); 
+		Thread consumer = new Consumer(buffer, maxSize, "CONSUMER"); 
+		producer.start(); 
+		consumer.start(); 
+	} 
+} 
+/** 
+* Producer Thread will keep producing values for Consumer 
+* to consumer. It will use wait() method when Queue is full 
+* and use notify() method to send notification to Consumer 
+* Thread. 
+*/ 
+class Producer extends Thread { 
+	private Queue<Integer> queue; 
+    private int maxSize; 
+    
+    public Producer(Queue<Integer> queue, int maxSize, String name){ 
+    	super(name); 
+    	this.queue = queue; 
+    	this.maxSize = maxSize; 
+    } 
+	  @Override 
+	  public void run() { 
+	   while (true){ 
+	     synchronized (queue) { 
+	      while (queue.size() == maxSize) { 
+	       try { 
+	        System.out .println("队列满, " + "Producer thread waiting for " + "consumer to take something from queue"); 
+	        queue.wait(); 
+	       } catch (Exception ex) { 
+	        ex.printStackTrace(); } 
+	       } 
+	       Random random = new Random(); 
+	       int i = random.nextInt(); 
+	       System.out.println("Producing value : " + i); queue.add(i); queue.notifyAll(); 
+	      } 
+	     } 
+	    } 
+   } 
+/** 
+* Consumer Thread will consumer values form shared queue. 
+* It will also use wait() method to wait if queue is 
+* empty. It will also use notify method to send 
+* notification to producer thread after consuming values 
+* from queue. 
+* 
+*/ 
+class Consumer extends Thread { 
+	private Queue<Integer> queue; 
+    private int maxSize; 
+  
+  public Consumer(Queue<Integer> queue, int maxSize, String name){ 
+   super(name); 
+   this.queue = queue; 
+   this.maxSize = maxSize; 
+  } 
+  @Override 
+  public void run() { 
+   while (true) { 
+    synchronized (queue) { 
+     while (queue.isEmpty()) { 
+      System.out.println("Queue is empty," + "Consumer thread is waiting" + " for producer thread to put something in queue"); 
+      try { 
+       queue.wait(); 
+      } catch (Exception ex) { 
+       ex.printStackTrace(); 
+      } 
+     } 
+     System.out.println("Consuming value : " + queue.remove()); queue.notifyAll(); 
+    } 
+   } 
+  } 
+} 
