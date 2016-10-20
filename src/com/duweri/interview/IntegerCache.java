92987430524c0f91aedc0f47eb25011f07cfea8d
codingWang <379906411@qq.com>
2016-10-20 21:01:25 +0800

    add Integer Cache

---
+++ src/com/duweri/interview/IntegerCache.java
@@ -0,0 +1,26 @@
+package com.duweri.interview;
+/**
+ * http://www.importnew.com/18884.html
+ * @author ¶ÅÎ°
+ */
+public class IntegerCache {
+    public static void main(String... strings) {
+ 
+        Integer integer1 = 3;//Integer.valueOf(3);
+        Integer integer2 = 3;
+ 
+        if (integer1 == integer2)
+            System.out.println("integer1 == integer2");
+        else
+            System.out.println("integer1 != integer2");
+ 
+        Integer integer3 = 300;
+        Integer integer4 = 300;
+ 
+        if (integer3 == integer4)
+            System.out.println("integer3 == integer4");
+        else
+            System.out.println("integer3 != integer4");
+ 
+    }
+}
